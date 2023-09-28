1c166ceb683326e60152fea8e09f27de18db229e
ADAMCSIK <adamcsik@uniduna.hu>
2023-09-28 08:46:59 +0200

    04

---
+++ elmelet04.py
@@ -0,0 +1,23 @@
+# ciklus
+
+def osszeadas(a, b):
+    global kerulet
+    kerulet = 25
+    eredmeny = sum(a, b)
+    #pass
+    return eredmeny
+
+print(osszeadas(8, 99))
+print(kerulet)
+
+"""szamlalo = 1
+while szamlalo <= 5:
+    print(szamlalo)
+    szamlalo += 1
+    if szamlalo == 3:
+        break
+else:
+    print("Vége a ciklusnak")
+
+print("Vége a programnak")
+"""
